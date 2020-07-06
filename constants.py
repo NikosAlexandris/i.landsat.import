6cb255b12a2510e0f56dcd6550f936380be55b7a
Nikos Alexandris <nik@nikosalexandris.net>
2020-07-06 15:19:57 +0200

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+MONTHS = {'01': 'jan', '02': 'feb', '03': 'mar', '04': 'apr', '05': 'may',
+          '06': 'jun', '07': 'jul', '08': 'aug', '09': 'sep', '10': 'oct',
+          '11': 'nov', '12': 'dec'}
+
+DATE_STRINGS = ['DATE_ACQUIRED', 'ACQUISITION_DATE']
+TIME_STRINGS = ['SCENE_CENTER_TIME', 'SCENE_CENTER_SCAN_TIME']
+ZERO_TIMEZONE = '+0000'
+GRASS_VERBOSITY_LELVEL_3 = '3'
+IMAGE_QUALITY_STRINGS = ['QA', 'VCID']
+QA_STRING = 'QA'  # Merge with above?  # FIXME
+MTL_STRING = 'MTL'
+HORIZONTAL_LINE = 79 * '-' + '\n'
+MEMORY_DEFAULT = '300'
+
