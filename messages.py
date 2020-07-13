a9f8117e8be1ae269a5b10054cde77c782f16791
Nikos Alexandris <nik@nikosalexandris.net>
2020-07-13 12:01:23 +0200

    Add messages.py

---
+++ messages.py
@@ -0,0 +1,2 @@
+MESSAGE_UNKNOWN_LANDSAT_IDENTIFIER='The identifier {scene} does not match any known Landsat product file name pattern!'
+MESSAGE_LIST_TIMESTAMPS_HEADLINE='Date\t\tTime\t\tTimezone\n'
